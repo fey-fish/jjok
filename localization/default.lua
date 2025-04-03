1aa7997b8d88da991fa668243c2a06796c431261
fey-fish <tmarshall1309@gmail.com>
2025-04-03 09:22:05 +0100

    Add files via upload

---
+++ localization/default.lua
@@ -0,0 +1,10 @@
+return {
+    misc = {
+        dictionary={
+            k_common="Grade 4",
+            k_uncommon="Grade 3",
+            k_rare="Grade 2",
+            k_legendary="Grade 1",
+        }
+    }
+}
