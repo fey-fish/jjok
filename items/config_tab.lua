da5da343c6bad17b4d5058d9776f0e552d61c29e
fey-fish <tmarshall1309@gmail.com>
2025-08-18 08:48:20 +0100

    modified:   items/config_tab.lua

--- items/config_tab.lua
+++ items/config_tab.lua
@@ -22,8 +22,37 @@ Jjok.config_tab = function()
                                 n = G.UIT.C,
                                 config = { align = "tm", padding = 0.03 },
                                 nodes = {
-                                    { n = G.UIT.R, config = { padding = 0.03 }, nodes = { create_toggle { col = true, label = 'Thatoru', scale = 1, w = 0, shadow = true, ref_table = Jjok.config, ref_value = "thatoru" } } },
-                                    { n = G.UIT.R, config = { padding = 0.03 }, nodes = { create_toggle { col = true, label = 'Majito', scale = 1, w = 0, shadow = true, ref_table = Jjok.config, ref_value = "majito" } } },
+                                    {
+                                        n = G.UIT.C,
+                                        config = {
+                                            ref_table = card,
+                                            align = "cr",
+                                            padding = 0.1,
+                                            r = 0.08,
+                                            minw = 1.25,
+                                            hover = true,
+                                            shadow = true,
+                                            colour = G.C.JJOK.LBLUE,
+                                            button = 'jjok_textures',
+                                            func = 'can_sell_card'
+                                        },
+                                        nodes = {
+                                            { n = G.UIT.B, config = { w = 0.1, h = 0.6 } },
+                                            {
+                                                n = G.UIT.C,
+                                                config = { align = "tm" },
+                                                nodes = {
+                                                    {
+                                                        n = G.UIT.R,
+                                                        config = { align = "cm", maxw = 1.25 },
+                                                        nodes = {
+                                                            { n = G.UIT.T, config = { text = 'Textures', colour = G.C.UI.TEXT_LIGHT, scale = 0.8, shadow = true } }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    },
                                 }
                             },
                             {
@@ -36,6 +65,31 @@ Jjok.config_tab = function()
                             },
                         }
                     },
+                }
+            }
+        }
+    }
+end
+
+function G.FUNCS.jjok_textures()
+    G.SETTINGS.paused = true
+    G.FUNCS.overlay_menu {
+        definition = textures(),
+    }
+end
+
+function textures()
+    return {
+        n = G.UIT.ROOT,
+        config = { align = "cm", r = 0.1, padding = 0.1, colour = G.C.JJOK.LBLUE, minw = 8, maxh = 6 },
+        nodes = {
+            {
+                n = G.UIT.C,
+                config = { align = 'cm', r = 0.03, padding = 0.01, colour = G.C.BLUE, minw = 6, maxh = 5 },
+                nodes = {
+                    { n = G.UIT.R, config = { padding = 0.03 }, nodes = { create_toggle { col = true, label = 'Thatoru', scale = 1, w = 0, shadow = true, ref_table = Jjok.config, ref_value = "thatoru" } } },
+                    { n = G.UIT.R, config = { padding = 0.03 }, nodes = { create_toggle { col = true, label = 'Majito', scale = 1, w = 0, shadow = true, ref_table = Jjok.config, ref_value = "majito" } } },
+
                 }
             }
         }
